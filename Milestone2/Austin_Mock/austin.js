80975dd243f971a4754c047932d08d08b662b120
Srivastava <SRIVASPM@legal.regn.net>
2017-10-17 20:38:09 -0400

    added mocking test and restructured directories

---
+++ Milestone2/Austin_Mock/austin.js
@@ -0,0 +1,29 @@
+var Promise = require("bluebird");
+var _ = require("underscore");
+var request = require("request");
+var querystring = require('querystring');
+
+var urlRoot = "https://api.austinbot.com";
+
+function getSprints()
+{
+    var options = {
+        url: urlRoot + "/sprint",
+        method: "GET",
+        headers: {
+            "content-type": "application/json"
+        }
+    };
+    
+    return new Promise(function (resolve, reject)
+    {
+        request(options, function(error, response, body)
+        {
+            var obj = JSON.parse(body);
+            console.log(obj);
+            resolve(obj);
+        });
+    });
+}
+
+exports.getSprints = getSprints;
---
+++ Milestone2/Austin_Mock/main.js
@@ -0,0 +1,30 @@
+var Promise = require("bluebird");
+var chai = require("chai");
+var expect = chai.expect;
+var nock = require("nock");
+var _ = require("underscore");
+
+var austin = require("./austin.js");
+
+//function to get the number of sprints
+function findNumberOfSprints()
+{
+	return new Promise(function (resolve, reject) 
+	{
+		// mock data needs list of issues.
+		austin.getSprints().then(function (sprints) 
+		{
+			console.log("hi");
+			console.log(sprints.length);
+            var sprint_count = sprints.length;
+			
+			resolve({sprint_count: sprint_count});
+		});
+	});
+}
+
+function dummy(){
+	
+}
+
+exports.findNumberOfSprints = findNumberOfSprints;
