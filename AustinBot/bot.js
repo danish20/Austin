228f7b77f2d139ada8d6fc9c1c339db5d744d5f6
Danish Suri <dsuri@ncsu.edu>
2017-10-21 13:32:45 -0400

    Json and message formats

--- AustinBot/bot.js
+++ AustinBot/bot.js
@@ -48,8 +48,47 @@ controller.hears('new sprint',['mention', 'direct_mention','direct_message'], fu
 
 controller.hears('hello',['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
-  console.log(message);
-  bot.reply(message,'## Hello, How are you?');
+  var jsonData = {
+    "attachments": [
+        {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+    ]
+};
+
+var responseImage = {
+  "attachments": [
+      {
+          "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
+          "title": "Network traffic (kb/s)",
+          "title_link": "https://datadog.com/path/to/event",
+          "text": "How does this look? @slack-ops - Sent by Julie Dodd",
+          "image_url": "https://d30y9cdsu7xlg0.cloudfront.net/png/327992-200.png",
+          "color": "#764FA5"
+      }
+  ]
+};
+  bot.reply(message,responseImage);
 });
 
 
