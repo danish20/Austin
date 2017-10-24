7404857757a6618f9e3b864c12e795b61c8c6a86
spilani <spilani@ncsu.edu>
2017-10-24 18:31:54 -0400

    Update user_performance.py

--- Milestone2/Python/Scripts/user_performance.py
+++ Milestone2/Python/Scripts/user_performance.py
@@ -6,20 +6,37 @@ import plotly.graph_objs as go
 import plotly
 import json
 from pprint import pprint
+import sys
+import s3
+import os
 
-#import tinys3
+current_path = os.path.dirname(os.path.realpath("__file__"))
+
+
+
+username = "Sandeep"
+sprintId = "20"
 
 plotly.tools.set_credentials_file(username='udeshmu', api_key='qIyD3uwDHJdtNvjSsFyS')
 
-import os
-current_path = os.path.dirname(os.path.realpath("__file__"))
-
 userPerf = {}
-with open(os.path.join(current_path,'../../Austin_Mock/mockData.json')) as data_file:
+with open(os.path.join(current_path,'../../../AustinBot/mockData.json')) as data_file:
     data = json.load(data_file)
-for story in data["sprint"][0]["stories"]:
+
+
+sprintIdx = 0
+userId = 0
+for idx, sprint in enumerate(data["sprint"]):
+    if sprint["id"] == sprintId:
+        sprintIdx = idx
+        for user in sprint["team_member"]:
+            if user["user_name"] == username:
+                userId = user["user_id"]
+
+
+for story in data["sprint"][sprintIdx]["stories"]:
     for task in story["task"]:
-        if task["user_id"] == 103:
+        if task["user_id"] == userId:
             if task["task_id"] not in userPerf:
                 sum = 0
                 for i in task["daily_progress"]: sum = sum + i["work_done"]
@@ -33,13 +50,29 @@ trace1 = go.Bar(
 
 data=[trace1]
 
-fig = dict(data=data)
-plotly.offline.plot(fig, filename='simple-connectgaps', image='png')
+layout = go.Layout(
+    title='User Performance Chart',
+    xaxis=dict(
+        title='Task Ids',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#7f7f7f'
+        )
+    ),
+    yaxis=dict(
+        title='No. of Hours',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#7f7f7f'
+        )
+    )
+)
+fig = dict(data=data, layout = layout)
+plotly.offline.plot(fig, filename='simple-connectgaps.html', image='png')
 
 py.image.save_as(fig, filename=os.path.join(current_path,'../Plots/user_performance.png'))
+s3.save_file_to_s3('user_performance.png')
 
 
-# conn = tinys3.Connection('AKIAIHCDSNE43LJH65VA','xEvn6MkKojhZ+znB4YF01D4d2LzkPByUO5i4u+7M',tls=True)
-
-# f = open('../Plots/plot_image.png','rb')
-# conn.upload('user_performance.png',f,'austinbot')
