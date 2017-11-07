d8218fcdea207961850f77f76fec20dd525bf5b4
Udit Deshmukh1 <udeshmukh1@Lenovo.com>
2017-11-06 20:06:29 -0500

    Refactored compare_user_performance.py to make it compatible for node

--- Milestone3/Python/Scripts/compare_user_performance.py
+++ Milestone3/Python/Scripts/compare_user_performance.py
@@ -9,58 +9,80 @@ from pprint import pprint
 import os
 import s3
 
-current_path = os.path.dirname(os.path.realpath("__file__"))
-file = open(os.path.join(current_path,'../../../AustinBot/mockData.json'), 'r')
-mock = json.load(file)
+def parse_json_for_compare_user_performance(query_id):
+    current_path = os.path.dirname(os.path.realpath("__file__"))
+    os.chdir(current_path)
+    #Traverse to the Project Root
+    #This is done by checking whether the folder AustinBot exits in the current path
+    while(not os.path.exists('AustinBot')):
+        current_path = os.path.join(current_path, '..')
+        os.chdir('..')
+    
+    file = open(os.path.join(current_path,'AustinBot/mockData.json'), 'r')
+    mock = json.load(file)
+    sprints = mock["sprint"]
+    for sprint in sprints:
+        if sprint["id"]==query_id:
+            current_sprint = sprint
 
-sprints = mock["sprint"]
-query_id="20"
+    expected = dict()
+    actual_work_done = dict()
 
-for sprint in sprints:
-    if sprint["id"]==query_id:
-        current_sprint = sprint
-
-expected = dict()
-actual_work_done = dict()
-
-for story in current_sprint["stories"]:
-    for task in story["task"]:
-        if task["status"]=="Complete":
-            try:
-                expected[task["user_id"]]+=task["excepted_hours"]
-            except:
-                expected[task["user_id"]]=task["excepted_hours"]
-            for day in task["daily_progress"]:
+    for story in current_sprint["stories"]:
+        for task in story["task"]:
+            if task["status"]=="Complete":
                 try:
-                    actual_work_done[task["user_id"]]+=day["work_done"]
+                    expected[task["user_id"]]+=task["excepted_hours"]
                 except:
-                    actual_work_done[task["user_id"]]=day["work_done"]
+                    expected[task["user_id"]]=task["excepted_hours"]
+                for day in task["daily_progress"]:
+                    try:
+                        actual_work_done[task["user_id"]]+=day["work_done"]
+                    except:
+                        actual_work_done[task["user_id"]]=day["work_done"]
 
 
-y_expected=list(expected.values())
-y_actual_work_done=list(actual_work_done.values())
-x=list(expected.keys())
+    y_expected=list(expected.values())
+    y_actual_work_done=list(actual_work_done.values())
+    x=list(expected.keys())
+    return [x, y_expected, y_actual_work_done]
 
-plotly.tools.set_credentials_file(username='spilani', api_key='XttFdFvm9v44kZTXOcWR')
+def plot_compare_user_performance(x, y_expected, y_actual_work_done):
+    plotly.tools.set_credentials_file(username='spilani', api_key='XttFdFvm9v44kZTXOcWR')
 
-trace1 = go.Bar(
-    x=x,
-    y=y_expected,
-    name='Expected Hours'
-)
-trace2 = go.Bar(
-    x=x,
-    y=y_actual_work_done,
-    name='Actual hours'
-)
+    current_path = os.path.dirname(os.path.realpath("__file__"))
+    os.chdir(current_path)
+    #Traverse to the Project Root
+    #This is done by checking whether the folder AustinBot exits in the current path
+    while(not os.path.exists('AustinBot')):
+        current_path = os.path.join(current_path, '..')
+        os.chdir('..')
 
-data = [trace1, trace2]
-layout = go.Layout(
-    barmode='group'
-)
+    trace1 = go.Bar(
+        x=x,
+        y=y_expected,
+        name='Expected Hours'
+    )
+    trace2 = go.Bar(
+        x=x,
+        y=y_actual_work_done,
+        name='Actual hours'
+    )
+    data = [trace1, trace2]
+    layout = go.Layout(
+        barmode='group'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    py.image.save_as(fig, filename=os.path.join(current_path,'Milestone3/Python/Plots/compare_user_performance.png'))
+    s3.save_file_to_s3('compare_user_performance.png')
 
-fig = go.Figure(data=data, layout=layout)
+def main():
+    query_id = "20"
+    [x, y_expected, y_actual_work_done] = parse_json_for_compare_user_performance(query_id)
+    fig = plot_compare_user_performance(x, y_expected, y_actual_work_done)
+    s3.save_file_to_s3('compare_user_performance.png')
+    print("comp")
 
-py.image.save_as(fig, filename=os.path.join(current_path,'../Plots/compare_user_performance.png'))
+if __name__ == '__main__':
+    main() 
 
-s3.save_file_to_s3('compare_user_performance.png')
