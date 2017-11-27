eb53e84df5063482b0c82cc3aa49f787237c1439
Danish Suri <dsuri@ncsu.edu>
2017-11-26 19:55:42 -0500

    Testing error

--- AustinBot/austin.js
+++ AustinBot/austin.js
@@ -3,7 +3,7 @@ var _ = require("underscore");
 var request = require("request");
 var querystring = require('querystring');
 
-var urlRoot = process.env.EXPRESS_URL+"/api";
+var urlRoot = process.env.EXPRESS_URL+":3001/api";
 var urlGithubRoot = "https://github.ncsu.edu/api/v3";
 const GITHUB_TOKEN = "token YOUR_TOKEN_HERE"
 
