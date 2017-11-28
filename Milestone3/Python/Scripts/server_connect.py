df37a613d7a28454d1f50f94da4c4b5751659068
Danish Suri <dsuri@ncsu.edu>
2017-11-28 00:02:44 -0500

    New Server

--- Milestone3/Python/Scripts/server_connect.py
+++ Milestone3/Python/Scripts/server_connect.py
@@ -1,7 +1,7 @@
 import urllib3
 import os
 
-URL = 'http://ec2-54-186-64-204.us-west-2.compute.amazonaws.com:3001/api/'
+URL = 'http://34.209.179.187:3001/api/'
 def fetch_data():
     http = urllib3.PoolManager()
     r = http.request('GET', URL+'sprint')
